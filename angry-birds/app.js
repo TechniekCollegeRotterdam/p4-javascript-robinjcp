1f98c5719287ef970c0d43705ec4875968e70714
Robin <robinpeereboom04@gmail.com>
2022-05-20 12:08:55 +0200

    ab

--- angry-birds/app.js
+++ angry-birds/app.js
@@ -8,3 +8,26 @@ bird.addEventListener("click", function(){
     //aan de bird class wordt styling toegevoegd, de styling is left 50px
     bird.style.left = count + "px";
 })
+
+window.addEventListener("keydown", function(e){
+    if(e.key  == "ArrowLeft"){
+        count -=50;
+        bird.style.left = count + "px";
+
+    }if(e.key  == "ArrowRight"){
+        count +=50;
+        bird.style.right = count + "px";
+    }
+
+    if(e.key  == "ArrowUp"){
+        count +=50;
+        bird.style.top = count + "px";
+    }
+
+    if(e.key  == "ArrowDown"){
+        count +=50;
+        bird.style.bottom = count + "px";
+    }
+
+
+})
