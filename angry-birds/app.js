e49e9eef49edbf7e06e0db697646d9d665278d73
Robin <robinpeereboom04@gmail.com>
2022-06-03 11:09:58 +0200

    ab game

--- angry-birds/app.js
+++ angry-birds/app.js
@@ -13,21 +13,26 @@ window.addEventListener("keydown", function(e){
     if(e.key  == "ArrowLeft"){
         count -=50;
         bird.style.left = count + "px";
+        <img src="img/b-left.svg" class="bird" alt="angry-bird"></img>
 
     }if(e.key  == "ArrowRight"){
         count +=50;
         bird.style.right = count + "px";
+        <img src="img/b-right.svg" class="bird" alt="angry-bird"></img>
     }
 
     if(e.key  == "ArrowUp"){
         count +=50;
         bird.style.top = count + "px";
+        <img src="img/b-up.svg" class="bird" alt="angry-bird"></img>
     }
 
     if(e.key  == "ArrowDown"){
         count +=50;
         bird.style.bottom = count + "px";
+        <img src="img/b-down.svg" class="bird" alt="angry-bird"></img>
     }
+
 
 
 })
